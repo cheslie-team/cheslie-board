df5f9b76289f433a3d9e57c45da2261de85e9e68
Teodor Ande Elstad <teodor@andeelstad.com>
2017-03-19 22:41:42 +0100

    Update name based on hash

--- client/client.js
+++ client/client.js
@@ -1,5 +1,8 @@
-var game = io('http://192.168.0.24:3000'),
-    board = ChessBoard('board');
+var game = io('http://localhost:3000'),
+    board = ChessBoard('board'),
+    gameId = window.location.hash.replace('#', '');
+
+document.getElementById('heading').innerHTML = gameId || 'cheslie-board';
 
 game.on('connect', function () {
     console.log("Connected to board");
@@ -8,7 +11,7 @@ game.on('connect', function () {
 
 game.on('move', function (move) {
     console.log(move);
-    if (move.gameId === window.location.hash.replace('#', '')) {
+    if (move.gameId === gameId) {
         ChessBoard('board', move.board);
     }
 });
