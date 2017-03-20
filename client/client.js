3fcd8db13426d00414f0837f5c1acc44fe74e8dd
Teodor Ande Elstad <teodor@andeelstad.com>
2017-03-20 23:07:02 +0100

    Show chessbord with start position before moves are made

--- client/client.js
+++ client/client.js
@@ -1,16 +1,14 @@
 var game = io('http://localhost:3000'),
-    board = ChessBoard('board'),
+    board = ChessBoard('board', 'start'),
     gameId = window.location.hash.replace('#', '');
 
 document.getElementById('heading').innerHTML = gameId || 'cheslie-board';
 
 game.on('connect', function () {
-    console.log("Connected to board");
     game.emit('subscribe');
 });
 
 game.on('move', function (move) {
-    console.log(move);
     if (move.gameId === gameId) {
         ChessBoard('board', move.board);
     }
